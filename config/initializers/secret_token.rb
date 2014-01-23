c9b4e25b8982f25f83a0f677ade70a597dc70dad
Erin Francis <ezzzaf@gmail.com>
2014-01-23 22:09:22 +1100

    rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'f13747d2397def880a47184cb2a93bdcbb05f37bf50117e35ebd8538769b84212a24f3de0729a2e1b304a2b6ba05f3f88497c2d0be15828688d8ce1d4b92db14'
